0def7c430684c56aa5e348695abb996cdae90714
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-15 20:06:24 +0300

    Add JwtConfigure

---
+++ src/main/kotlin/ru/tenzou/tsodgis/security/jwt/JwtConfigure.kt
@@ -0,0 +1,15 @@
+package ru.tenzou.tsodgis.security.jwt
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class JwtConfigure(private val jwtTokenProvider: JwtTokenProvider) :
+    SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+        override fun configure(httpSecurity: HttpSecurity) {
+            val jwtTokenFilter = JwtTokenFilter(jwtTokenProvider)
+            httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+        }
+}
