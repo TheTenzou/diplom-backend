00338ed068cb4728d6458fc11477e088ded9f096
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-15 20:21:52 +0300

    Add Security config

---
+++ src/main/kotlin/ru/tenzou/tsodgis/config/SecurityConfig.kt
@@ -0,0 +1,40 @@
+package ru.tenzou.tsodgis.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import ru.tenzou.tsodgis.security.jwt.JwtConfigure
+import ru.tenzou.tsodgis.security.jwt.JwtTokenProvider
+
+@Configuration
+class SecurityConfig @Autowired constructor(
+    private val jwtTokenProvider: JwtTokenProvider
+) :
+    WebSecurityConfigurerAdapter() {
+
+    companion object {
+        private const val ADMIN_ENDPOINT = "/api/v1/admin/*"
+        private const val LOGIN_ENDPOINT = "/api/v1/auth/login"
+    }
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    override fun configure(http: HttpSecurity?) {
+        http?.httpBasic()?.disable()
+            ?.csrf()?.disable()
+            ?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            ?.and()
+            ?.authorizeRequests()
+            ?.antMatchers(ADMIN_ENDPOINT)?.permitAll()
+            ?.antMatchers(LOGIN_ENDPOINT)?.hasRole("ADMIN")
+            ?.and()
+            ?.apply(JwtConfigure(jwtTokenProvider))
+    }
+}
