a1d66e370fabc7fb0b0b37d0be5a40921df59af1
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-15 18:59:19 +0300

    Add JwtUserDetailsService

---
+++ src/main/kotlin/ru/tenzou/tsodgis/security/JwtUserDetailsService.kt
@@ -0,0 +1,27 @@
+package ru.tenzou.tsodgis.security
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import ru.tenzou.tsodgis.service.UserService
+import ru.tenzou.tsodgis.service.impl.UserServiceImpl
+
+@Service
+class JwtUserDetailsService @Autowired constructor(private val userService: UserService) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val user = username?.let { userService.findByUsername(it) }
+            ?: throw UsernameNotFoundException("User with username: $username not found")
+
+        logger.info("IN loadUserByUsername - user with username: $username successfully loaded")
+        return UserDetailsJwt.create(user)
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(JwtUserDetailsService::class.java)
+    }
+}
